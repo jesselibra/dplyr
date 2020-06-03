6080def910c76d1b1492e8bd803aac882732a925
jesselibra <jessemlibra@gmail.com>
2020-06-02 20:25:36 -0500

    notes

--- dplyr_mexico_city.R
+++ dplyr_mexico_city.R
@@ -6,48 +6,54 @@ data<- data.frame()
 z<-c(2016,2017,2018,2019,2020)
 for(i in z) {
   url <- paste("http://www.aire.cdmx.gob.mx/opendata/promedios_diarios/promedios_",i,"_ps.csv", sep = "")
-  path <- paste("C:/Users/jesse/Desktop/R/dplyr",i, sep = "")
+  path <- paste("C:/Users/jesse/Desktop/R/dplyr/data/",i, sep = "")
   download.file(url, path)
   data <- rbind(data, read.csv(path,header = T, skip = 8))
 }
-
-pm<- tibble(data)
-rm(data)
+##create tibble and summary of tibble
+pm <- tibble(data)
 summary(pm)
 
-
-##clean
+##clean pm using dplyr functions
 pmclean <- pm %>%
-  mutate(datex = strptime(date,"%d/%m/%Y"), month = month(datex), year = year(datex)) %>%
+  mutate(datex = strptime(date,"%d/%m/%Y"), 
+         month = month(datex), 
+         year = year(datex)) %>%
   filter(!is.na(value)) %>%
   select(datex, id_station:value, month, year) %>%
   rename(date = datex)
-
+##create a tibble giving summary datat by month and year
 pmsummary <- pmclean %>%  
   group_by(id_parameter, year, month) %>%
   summarize("mean" = mean(value), "median" = median(value), 
             "min" = min(value), "max" = max(value))
 
-plot(pmsummary$month,pmsummary$mean, 
-     col = ifelse(pmsummary$year == "2020", "coral1", "grey"),
-     pch = ifelse(pmsummary$id_parameter == 'PM10', 19,2),
-     xlab = "Month",
-     ylab = "PM Mean Concentration (ug/m3)",
-     cex = 2,
-     type ="p" )
-legend("bottomright", c("PM10", "PM2.5", "2020"),
+    ##Graph the summary data
+    plot(pmsummary$month,pmsummary$mean, 
+       col = ifelse(pmsummary$year == "2020", "coral1", "grey"),
+      pch = ifelse(pmsummary$id_parameter == 'PM10', 19,2),
+      xlab = "Month",
+      ylab = "PM Mean Concentration (ug/m3)",
+      cex = 2,
+      type ="p" )
+    legend("bottomright", c("PM10", "PM2.5", "2020"),
        col = c("grey","grey", "coral1"),
        pch = c(20, 2, 1))
 
-plot(pmsummary$month ~ pmsummary$mean)
+## download Mexico City air quality monitoring station data
+url <- "http://www.aire.cdmx.gob.mx/opendata/catalogos/cat_estacion.csv"
+path <- "C:/Users/jesse/Desktop/R/dplyr/stations.csv"
+download.file(url, path)
+datastations <- tibble(read.csv(path,header = T, skip = 1))
 
+##select columns and rename our join column
+stations <- datastations %>%
+   select(cve_estac:obs_estac)%>%
+   rename(id_station = cve_estac)
 
-pm10 <- pmclean %>%
-  filter(id_parameter == "PM10") %>%
-  select(date,id_station, value, month, year)
-pm2.5 <- pmclean %>%
-  filter(id_parameter == "PM2.5") %>%
-  select(date, id_station, value, month, year)
+##join clean PM data with station data
+pmstations<- inner_join(pmclean,stations)
 
-
+##use arrange and desc to identify inconsistencies. 
+arrange(pmstations,desc(obs_estac))
 
