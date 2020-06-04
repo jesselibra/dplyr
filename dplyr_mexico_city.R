d74cb0140ef2e94a1bd26edcea334dd6bebb2113
jesselibra <jessemlibra@gmail.com>
2020-06-04 09:37:15 -0500

    Clean code

--- dplyr_mexico_city.R
+++ dplyr_mexico_city.R
@@ -2,13 +2,11 @@ library(dplyr)
 library(lubridate)
 
 ##getting the data
-data<- data.frame()
-z<-c(2016,2017,2018,2019,2020)
-for(i in z) {
-  url <- paste("http://www.aire.cdmx.gob.mx/opendata/promedios_diarios/promedios_",i,"_ps.csv", sep = "")
-  path <- paste("C:/Users/jesse/Desktop/R/dplyr/data/",i, sep = "")
-  download.file(url, path)
-  data <- rbind(data, read.csv(path,header = T, skip = 8))
+  data<- data.frame()
+  z<-c(2016,2017,2018,2019,2020)
+  for(i in z) {
+    url <- paste("http://www.aire.cdmx.gob.mx/opendata/promedios_diarios/promedios_",i,"_ps.csv", sep = "")
+    data <- rbind(data, read.csv(url,header = T, skip = 8))
 }
 ##create tibble and summary of tibble
 pm <- tibble(data)
@@ -22,12 +20,20 @@ pmclean <- pm %>%
   filter(!is.na(value)) %>%
   select(datex, id_station:value, month, year) %>%
   rename(date = datex)
+
 ##create a tibble giving summary datat by month and year
 pmsummary <- pmclean %>%  
   group_by(id_parameter, year, month) %>%
   summarize("mean" = mean(value), "median" = median(value), 
             "min" = min(value), "max" = max(value))
 
+  ##PM10
+  arrange(filter(pmsummary,id_parameter == "PM10" & 
+                   month %in% 1:4),month, mean)
+  ##PM2.5
+  arrange(filter(pmsummary,id_parameter == "PM2.5" & 
+                   month %in% 1:4),month, mean)
+  
     ##Graph the summary data
     plot(pmsummary$month,pmsummary$mean, 
        col = ifelse(pmsummary$year == "2020", "coral1", "grey"),
@@ -42,18 +48,13 @@ pmsummary <- pmclean %>%
 
 ## download Mexico City air quality monitoring station data
 url <- "http://www.aire.cdmx.gob.mx/opendata/catalogos/cat_estacion.csv"
-path <- "C:/Users/jesse/Desktop/R/dplyr/stations.csv"
-download.file(url, path)
-datastations <- tibble(read.csv(path,header = T, skip = 1))
+datastations <- tibble(read.csv(url,header = T, skip = 1))
 
-##select columns and rename our join column
-stations <- datastations %>%
+ ##select columns and rename our join column
+ stations <- datastations %>%
    select(cve_estac:obs_estac)%>%
    rename(id_station = cve_estac)
 
-##join clean PM data with station data
-pmstations<- inner_join(pmclean,stations)
-
-##use arrange and desc to identify inconsistencies. 
-arrange(pmstations,desc(obs_estac))
+ ##join clean PM data with station data
+ pmstations<- inner_join(pmclean,stations)
 
